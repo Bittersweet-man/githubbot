f215627248e4386d017d2d2157cd05eb7d193a77
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2019-01-19 15:59:32 -0700

    fbf

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const discord = require('discord.js')
 const bot = new discord.Client()
-bot.login('NTExMDA2MDU0NDk1Mjg5MzU0.DyU7Fw.tBWW4NRQTw2F3UE_HPTXqhIecvY')
+const TOKEN = process.env.TOKEN
+bot.login(TOKEN)
