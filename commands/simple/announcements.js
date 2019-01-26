8cce5a5cf254aff2e57623372a3f17379b7f6cce
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2019-01-26 13:46:26 -0700

    jf

--- commands/simple/announcements.js
+++ commands/simple/announcements.js
@@ -11,15 +11,17 @@ class AnnouncementsCommand extends Commando.Command {
     }
 
 
+
     async run(message, args) {
+        var role = message.guild.roles.find(role => role.name === "announcements")
         if (message.guild.id == 465707591910162432) {
-            if (message.member.roles.has('511266254615740437')) {
-                message.member.removeRole('511266254615740437')
+            if (message.member.roles.has(role)) {
+                message.member.removeRole(role)
                 message.reply("The announcements role has been removed.")
                 return;
             }
-            if (!message.member.roles.has('511266254615740437')) {
-                message.member.addRole('511266254615740437')
+            if (!message.member.roles.has(role)) {
+                message.member.addRole(role)
                 message.reply('You have recieved the announcements role, and will be notified of new announcements!')
             }
         } else {
