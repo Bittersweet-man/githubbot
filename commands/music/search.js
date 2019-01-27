a69ed8e685b83db306d59712127ed68a72cfb64a
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2019-01-26 21:58:10 -0700

    hd

--- commands/music/search.js
+++ commands/music/search.js
@@ -1,36 +1,33 @@
-const search = require('yt-search')
+const YouTube = require('simple-youtube-api')
 const Commando = require('discord.js-commando');
-
-class SearchCommand extends Commando.Command
-{
-    constructor(client,)
-        {
-        super(client,{
+const youtube = new YouTube(AIzaSyBPn0OXaz3_e0VaKs - ZGHEJ5eP15 - GrVoE);
+class SearchCommand extends Commando.Command {
+    constructor(client, ) {
+        super(client, {
             name: 'search',
             group: 'music',
             memberName: 'search',
             description: 'Search for a youtube video!'
         });
     }
-        
-        
-    async run(message, args)
-    {
 
-        search(args.join(' '), function(err, res){
-            if (err) return message.channel.send("Sorry, I can't do that right now.")
-            let videos = res.videos.slice(0, 10)
-            let resp = '';
-            for(var i in videos){
-                resp += `**[${parseInt(i)+1}]:** \'${videos[i].title}\'\n`
-            }
-            resp += `\n**Choose a number between \'1-${videos.length}\'`
-            message.channel.send(resp)
-            
-            }
-        
 
-        )}
+    async run(message, args) {
+        try {
+            var video = await youtube.getVideo(url)
+
+        } catch (error) {
+            try {
+var videos = await youtube.searchVideos(url, 1)
+var video2 = await youtube.getVideoByID(videos[0].id)
+message.channel.send(video2)
+            } catch (error) {
+return message.channel.send("there was an oopsie")
+            }
+        }
+
+
+    }
 }
 
 module.exports = SearchCommand;
