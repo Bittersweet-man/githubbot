bbe32ebd66214d94b30a7a4918628082b76c7195
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2019-01-27 18:25:17 -0700

    leave

--- commands/music/leave.js
+++ commands/music/leave.js
@@ -1,32 +1,26 @@
 const Commando = require('discord.js-commando');
 
-class LeaveChannelCommand extends Commando.Command
-{
-    constructor(client,)
-        {
-        super(client,{
+class LeaveChannelCommand extends Commando.Command {
+    constructor(client, ) {
+        super(client, {
             name: 'leave',
             group: 'music',
             memberName: 'leave',
             description: 'Leaves the voice channel of the commander'
         });
     }
-        
-        
-    async run(message, args)
-    {
-        if(message.guild.voiceConnection)
-        {
-            message.guild.voiceConnection.disconnect();        
-        }
-        else
-        {
-            message.reply("I must be in a voice channel to leave");
-        }
+
+
+    async run(message, args) {
+        if (!message.member.voiceChannel) return message.channel.send("Please connect to a voice channel.")
+        if (!message.guild.me.voiceChannel) return message.channel.send("Sorry, I'm not in a voice channel!")
+        if (message.guild.me.voiceChannel !== message.member.voiceChannel) return message.channel.send("Sorry, we aren't in the same voice channel!")
+        message.guild.me.voiceChannel.leave()
+        message.channel.send("Leaving...")
 
     }
-    
-    
+
+
 }
 
 module.exports = LeaveChannelCommand;
