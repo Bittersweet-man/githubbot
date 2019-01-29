bfe71a01f784b9af56ac9dab33e5fe926053c941
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2019-01-28 18:56:16 -0700

    fhf

--- commands/music/play.js
+++ commands/music/play.js
@@ -3,6 +3,18 @@ const ytdl = require('ytdl-core')
 const discord = require('discord.js')
 const bot = new discord.Client()
 
+
+function play (connection, message){
+    var server = servers[message.guild.id]
+    server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}))
+
+    server.queue.shift();
+server.dispatcher.on('end', function() {
+    if(server.queue[0]) play(connection, message)
+    else connection.disconnect
+})
+}
+
 class PlayCommand extends Commando.Command {
     constructor(client, ) {
         super(client, {
@@ -15,63 +27,24 @@ class PlayCommand extends Commando.Command {
 
 
     async run(message, args) {
-        if (!message.member.voiceChannel) return message.channel.send("You must be in a voice channel!")
-        if (!args) return message.channel.send("Sorry, please send a valid url with the command.")
-        let info = await ytdl.getInfo(args, (err, info) => {
-            if (err) return message.channel.sendMessage('Invalid YouTube Link: ' + err);
-        })
-        let data = active.get(message.guild.id) || {};
-        if (!data.connection) data.connection = await message.member.voiceChannel.join()
-        if (!data.queue) data.queue = [];
-        data.guildID = message.guild.id;
-        data.queue.push({
-            //songTitle: info.title,
-            requester: message.author.tag,
-            url: args,
-            announceChannel: message.channel.id
-        });
-
-        if (!data.dispatcher) play(bot, data);
-        else {
-            message.channel.send(`Added to queue: song | Requested By: ${message.author.id}`)
-            //replace "here" with ${info.title}
+        var servers = {};
+        if (!args[1]) {
+message.channel.send("Please provide a link")
+return
         }
-        active.set(message.guild.id, data)
-
-
-
-
-
-
-
-        async function play(data, bot) {
-            var bot = new discord.Client()
-           var channel = bot.channels.get('538707114781179904')
-           channel.send(`Now Playing: ${data.queue[0].songTitle} | Requested By: ${data.queue[0].requester}`)
-            data.dispatcher = await data.connection.playStream(ytdl(data.queue[0].url, {
-                filter: 'audioonly'
-            }));
-            data.dispatcher.guildID = data.guildID
-
-            data.dispatcher.once('end', function () {
-                end(this, bot)
-            })
+        if(!message.member.voiceChannel){
+message.channel.send("You must be in a VC")
+return
+        }
+        if(!servers[message.guild.id]) servers[message.guild.id] = {
+            queue: []
+        }
+        var server = servers[message.guild.id]
+        if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
+ play(connection, message);
         }
 
-        function end(bot, dispatcher) {
-            let fetched = active.get(dispatcher.guildID)
-            fetched.queue.shift();
-            if (fetched.queue.length > 0) {
-                active.set(dispatcher.guildID, fetched)
-                play(bot, fetched);
-            } else {
-                active.delete(dispatcher.guildID)
-                let vc = bot.guilds.get(dispatcher.guild.id).me.voiceChannel;
-                if (vc) vc.leave()
-
-            }
-        }
-    }
+        )}
 }
 
 module.exports = PlayCommand;
