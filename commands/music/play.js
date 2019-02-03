60b8321ad67a32fdba81df6ae5fa1a9df56f00d2
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2019-02-02 20:46:21 -0700

    dhd

---
+++ commands/music/play.js
@@ -0,0 +1,48 @@
+const Commando = require('discord.js-commando');
+var servers = {};
+const YTDL = require('ytdl-core');
+function play(connection, message){
+var server = servers[message.guild.id]
+server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}))
+server.queue.shift();
+server.dispatcher.on('end', function(){
+    if(server.queue[0]){
+        play(connection, message)
+
+    }
+    else connection.disconnect()
+})
+}
+
+class PlayCommand extends Commando.Command {
+    constructor(client, ) {
+        super(client, {
+            name: 'play',
+            group: 'music',
+            memberName: 'play',
+            description: 'Play any song!'
+        });
+    }
+
+
+    async run(message, args) {
+        if (!args) {
+            message.channel.send("Please provide a link!")
+            return;
+        }
+        if (!message.member.voiceChannel) {
+            message.channel.send("You must be in a voice channel!")
+            return;
+        }
+        if (!servers[message.guild.id]) servers[message.guild.id] = {
+            queue: []
+        }
+        var server = servers[message.guild.id]
+        if (!message.guild.voiceChannel) message.member.voicechannel.join().then(function (connection) {
+            play(connection, message)
+        })
+    }
+
+}
+
+module.exports = PlayCommand;
