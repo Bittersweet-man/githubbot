ad876803bafd65535212b0d073b6c3a6bd219151
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2019-02-12 18:42:04 -0700

    sgs

--- commands/music/play.js
+++ commands/music/play.js
@@ -70,9 +70,10 @@ module.exports = class PlayCommand extends commando.Command {
                 })
                 .catch(console.error);
         }
-let video = await api.searchVideos(args)
-let title = video.title
-console.log(title)
+        const api = new YouTube(process.env.YouTubeAPI);
+        let video = await api.searchVideos(args)
+        let title = video.title
+        console.log(title)
         // Push url to queue
         server.queue.push(url);
 
